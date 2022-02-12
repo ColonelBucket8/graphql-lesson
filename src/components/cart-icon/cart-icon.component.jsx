312b2a9500a8f5d24b032d228e44cd81a993e8b0
ColonelBucket8 <freedomterran89@gmail.com>
2022-02-12 22:40:12 +0800

    added carthidden mutation in header and cart-icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 import {
   CartIconContainer,
@@ -19,12 +18,8 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
   </CartIconContainer>
 );
 
-const mapDispatchToProps = (dispatch) => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden()),
-});
-
 const mapStateToProps = createStructuredSelector({
   itemCount: selectCartItemsCount,
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps)(CartIcon);
